0e2c283f77dab257df86f5e2f30f2f77055f49a6
awaisrao <awaisrao789@gmail.com>
2023-01-19 23:30:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,25 +16,25 @@ app = Flask(__name__)
 
 
 
-cursor = db.cursor()
+#cursor = db.cursor()
 
 
 
 @app.route("/",methods=['GET','POST'])
 def read():
-    if request.method == 'POST' and 'name' in request.form and 'student_class' in request.form and 'age' in request.form and 'address' in request.form:
-       nm = request.form['name']
-       sc = request.form['student_class']
-       ag = request.form['age']
-       addr = request.form['address']
-       cursor.execute("INSERT INTO students VALUES (%s,%s,%s,%s)",(nm,sc,ag,addr))
-    cursor.execute("SELECT * FROM students")
-    records = cursor.fetchall()
+#    if request.method == 'POST' and 'name' in request.form and 'student_class' in request.form and 'age' in request.form and 'address' in request.form:
+#       nm = request.form['name']
+#       sc = request.form['student_class']
+#       ag = request.form['age']
+#       addr = request.form['address']
+#       cursor.execute("INSERT INTO students VALUES (%s,%s,%s,%s)",(nm,sc,ag,addr))
+#    cursor.execute("SELECT * FROM students")
+#    records = cursor.fetchall()
     return render_template('index.html',data=records)
 
 @app.route('/delete/<string:name>', methods=['POST'])
 def remove(name):
-   cursor.execute("DELETE FROM students where name=%s",name)
+#   cursor.execute("DELETE FROM students where name=%s",name)
    return redirect (url_for('read'))
 
 if __name__ == "__main__":
